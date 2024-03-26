3036e656f636b8c019ef07646dc3caa732cbfd5d
LaurinC <laurinc100@gmail.com>
2024-03-26 12:44:56 +0100

    bbox coords

--- Kamera/object detection/yolo_test.py
+++ Kamera/object detection/yolo_test.py
@@ -1,6 +1,7 @@
 from ultralytics import YOLO
 from ultralytics.utils.plotting import Annotator
 import cv2 as cv
+import numpy as np
 
 """
 Example script for getting bounding boxes from YOLOv8
@@ -8,27 +9,32 @@ Example script for getting bounding boxes from YOLOv8
 Should camera interface be infinite loop or one-shot?
 """
 
-def one_shot(cap : cv.VideoCapture, model : YOLO, *args, **kwargs):
+def one_shot(cap : cv.VideoCapture, model : YOLO) -> dict | None:
     # grab image from camera
     ret, img = cap.read()
-    if ret is False: return
+    if ret is False: return None
     # TODO: undistort image
 
     # get yolo prediction
     results = model.predict(img, verbose = False)
     # annotate image with results
+    coords = []
     for r in results:
         annotator = Annotator(img)
         boxes = r.boxes
         for box in boxes:
             b = box.xyxy[0]
+            coords.append(b.numpy().astype(np.uint16))
             c = box.cls
             # label with distance, veloctiy, ... could be added here
             # TODO: 
             label = f'{model.names[int(c)]}'
             annotator.box_label(b, label)
-    img = annotator.result()
-    return img
+    annotated = {
+        'img' : annotator.result(),
+        'xyxy' : coords
+    }
+    return annotated
 
 if __name__ == '__main__':
     # initialize object detection model
@@ -37,12 +43,13 @@ if __name__ == '__main__':
     cap = cv.VideoCapture(0)
     # capture loop
     while True:
-        img = one_shot(cap, model)
-        # display annotated image
-        cv.imshow('Detection', img)
-        # check for loop end
-        key = cv.waitKey(1) 
-        if key & 0xFF == 27: break
+        annotated = one_shot(cap, model)
+        if annotated is not None:
+            # display annotated image
+            cv.imshow('Detection', annotated['img'])
+            # check for loop end
+            key = cv.waitKey(1) 
+            if key & 0xFF == 27: break
     # cleanup
     cap.release()
     cv.destroyAllWindows()
