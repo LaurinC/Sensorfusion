14de1d9017affdae26006ab9926d1de92cf954c5
LaurinC <laurinc100@gmail.com>
2024-07-01 10:57:14 +0200

    updated test script

--- test.py
+++ test.py
@@ -4,10 +4,14 @@ from fusion.utils import display_fusion
 if __name__ == '__main__':
     # options for com ports
     com = {
-        'conf_port': 'COM4',
+        'conf_port': '/dev/ttyUSB0',
         'conf_baud': 115200,
         'conf_to': 0.01,
-        'data_port': 'COM3',
+        'data_port': '/dev/ttyUSB1',
         'data_baud': 921600
     }
     fusion = Fusion(com, 'wide_lense1')
+
+    while True:
+        udst, points = fusion()
+        print(udst.shape)
